4747cf44a240c3ab7c55c552e62d4e45fafd6f24
sant865 <86.santiago@gmail.com>
2025-01-20 19:39:51 -0300

    refactoring reviews rating

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2025_01_20_155952) do
+ActiveRecord::Schema[7.0].define(version: 2025_01_20_221826) do
   create_table "active_storage_attachments", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
@@ -70,6 +70,8 @@ ActiveRecord::Schema[7.0].define(version: 2025_01_20_155952) do
     t.string "zip_code"
     t.integer "price_cents"
     t.string "price_currency"
+    t.integer "reviews_count"
+    t.decimal "average_rating", precision: 10
     t.index ["latitude", "longitude"], name: "index_properties_on_latitude_and_longitude"
   end
 
